954aa15df536c916636936598d3757cfd2068f77
musticide <mustafa25.tm@gmail.com>
2025-08-24 17:42:02 +0530

    Upgraded to opengl es3

--- app/src/main/java/com/example/bubbleshooter/MainActivity.java
+++ app/src/main/java/com/example/bubbleshooter/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.bubbleshooter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
 import android.os.Bundle;
 import android.opengl.GLSurfaceView;
+import android.widget.Toast;
+import android.util.Log;
 
 //import com.example.bubbleshooter.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "GameRenderer";
     private GLSurfaceView glSurfaceView;
     // Used to load the 'bubbleshooter' library on application startup.
     static {
@@ -21,13 +27,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(!checkGLES30Support())
+        {
+            Log.e(TAG, "OpenGL ES 3.0 not supported on this device");
+            Toast.makeText(this, "OpenGL ES 3.0 Not supported", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
         glSurfaceView = new GLSurfaceView(this);
-        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setEGLContextClientVersion(3);
 
         GameRenderer renderer = new GameRenderer(this);
         glSurfaceView.setRenderer(renderer);
 
         setContentView(glSurfaceView);
+    }
+
+    private boolean checkGLES30Support(){
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+
+        return configurationInfo.reqGlEsVersion >= 0x30000;
     }
 
     @Override
